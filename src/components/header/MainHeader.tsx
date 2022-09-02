74a101bb4f794188552284ef3902270df8da8727
naniitevez <nicole.tevez.97@gmail.com>
2022-09-02 05:51:32 -0300

    header sticky

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LogoDesktop from "./components/LogoDesktop";
 import MainMenu from "./components/MainMenu";
 import MenuMobile from "./components/MenuMobile";
 import PopupMinicart from "./components/PopupMinicart";
 
 const MainHeader: React.FC = () => {
+  const [OnTop, setOnTop] = useState<boolean>(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if (position < 3) {
+      setOnTop(true);
+    } else setOnTop(false);
+  };
+
   return (
-    <header id="prueba" className="transparent-header">
+    <header
+      id="prueba"
+      className={`${
+        OnTop ? "transparent-header" : "transparent-header sticky-header"
+      }`}>
       <div className="header-area box-90">
         <div className="container-fluid">
           <div className="row align-items-center">
             <div className="col-xl-2 col-lg-6 col-md-6 col-7 col-sm-3 d-flex align-items-center">
-              <LogoDesktop logo="/images/Click-logo.png"/>
+              <LogoDesktop logo="/images/Click-logo.png" />
             </div>
             <div className="col-xl-9 col-lg-8 col-md-10 col-10 d-none d-xl-block header-menu_wrapper">
               <MainMenu />
