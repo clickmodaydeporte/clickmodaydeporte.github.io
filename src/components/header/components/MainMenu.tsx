5d789a7795cee1bcba94e1c90790df69df6d17ec
naniitevez <nicole.tevez.97@gmail.com>
2022-09-02 16:36:24 -0300

    acomodo items verticalmente en menu de header

--- src/components/header/components/MainMenu.tsx
+++ src/components/header/components/MainMenu.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const MainMenu: React.FC = () => {
   return (
     <div className="main-menu">
-      <nav id="mobile-menu" style={{ display: "block" }}>
+      <nav id="menu" style={{ display: "block" }}>
         <ul>
           <li>
             <Link href="/">
