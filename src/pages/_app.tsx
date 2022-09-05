f2dbc3f0c34a9451b067812eb2785ddfd3708497
naniitevez <nicole.tevez.97@gmail.com>
2022-09-05 19:56:42 -0300

    agrego packajes para fontawesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import "../styles/bootstrap.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import type { AppProps } from "next/app";
 import MainLayout from "../components/MainLayout";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
