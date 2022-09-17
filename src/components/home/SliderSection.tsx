fc8b55819dffc7bc13b206779fd38a5ac76de63c
naniitevez <nicole.tevez.97@gmail.com>
2022-09-17 20:08:13 -0300

    agrego textos en las demas slides

--- src/components/home/SliderSection.tsx
+++ src/components/home/SliderSection.tsx
@@ -12,14 +12,14 @@ const SliderSection: React.FC = () => {
         interval={6000}
         transitionTime={1000}
         infiniteLoop
-        autoPlay
+        // autoPlay
       >
         <div className="item">
           <img
             src="/images/click-slider-3.jpg"
             // src="https://via.placeholder.com/1600x800"
             className="img-responsive"
-            alt=""
+            alt="imagen de portada de la tienda Click moda y deporte"
           />
           <div className="shape-title">
             <h2>2022</h2>
@@ -31,14 +31,14 @@ const SliderSection: React.FC = () => {
                   <span
                     data-animation="fadeInRight"
                     data-delay=".2s"
-                    className=""
+                    className="cover"
                     style={{ animationDelay: "0.2s" }}>
                     Click Moda y Deporte
                   </span>
                   <h1
                     data-animation="fadeInUp"
                     data-delay=".5s"
-                    className=""
+                    className="cover"
                     style={{ animationDelay: "0.5s" }}>
                     Creá tu propio estilo
                   </h1>
@@ -51,15 +51,30 @@ const SliderSection: React.FC = () => {
           <img
             src="/images/click-slider-4.jpg"
             className="img-responsive"
-            alt=""
+            alt="segunda imagen de portada de click Moda y Deporte"
           />
-        </div>
-        <div className="item">
-          <img
-            src="/images/click-slider-1.jpg"
-            className="img-responsive"
-            alt=""
-          />
+          <div className="container-fluid slide-text_wrapper">
+            <div className="row">
+              <div className="col-xl-9 second-item">
+                <div className="slide-content pt-200">
+                  <span
+                    data-animation="fadeInRight"
+                    data-delay=".2s"
+                    className="slide-2"
+                    style={{ animationDelay: "0.2s" }}>
+                    Click Moda y Deporte
+                  </span>
+                  <h2
+                    data-animation="fadeInUp"
+                    data-delay=".5s"
+                    className="slide-2"
+                    style={{ animationDelay: "0.5s" }}>
+                    Estilo es usar lo que te hace sentir bien
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="item">
           <img
@@ -67,6 +82,50 @@ const SliderSection: React.FC = () => {
             className="img-responsive"
             alt=""
           />
+          <div className="container-fluid slide-text_wrapper">
+            <div className="row">
+              <div className="col-xl-9 third-item">
+                <div className="slide-content pt-200">
+                  <span
+                    data-animation="fadeInRight"
+                    data-delay=".2s"
+                    className="slide-3"
+                    style={{ animationDelay: "0.2s" }}>
+                    Click Moda y Deporte
+                  </span>
+                  <h2
+                    data-animation="fadeInUp"
+                    data-delay=".5s"
+                    className="slide-3"
+                    style={{ animationDelay: "0.5s" }}>
+                    Comodidad en tu entrenamiento.
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img
+            src="/images/click-slider-1.jpg"
+            className="img-responsive"
+            alt=""
+          />
+          <div className="container-fluid slide-text_wrapper">
+            <div className="row">
+              <div className="col-xl-9 fourth-item">
+                <div className="slide-content pt-200">
+                  <h2
+                    data-animation="fadeInUp"
+                    data-delay=".5s"
+                    className="slide-4"
+                    style={{ animationDelay: "0.5s" }}>
+                    Click Moda y Deporte
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Carousel>
     </div>
