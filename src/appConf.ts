a0708b4e8ab344ed5df09eca03c1cbb13b9f8a89
naniitevez <nicole.tevez.97@gmail.com>
2022-09-20 03:19:40 -0300

    creo archivo appConfig

---
+++ src/appConf.ts
@@ -0,0 +1,5 @@
+export const DASHBOARD_ID = "1OUZSMH6SKMwwWwAyayNm-g7f3P6NPM-AKBHHHPUfF-k";
+
+export const GOOGLE_APIKEY = "AIzaSyBKaCT0lI2ojTzwEDesK9w_gkEfpRofvAU";
+
+export const IMG_URL = "https://drive.google.com/uc?export=view&id=";
