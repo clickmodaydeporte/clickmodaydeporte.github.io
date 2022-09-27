844807312700b4811dc38da477469f8df55e2ceb
naniitevez <nicole.tevez.97@gmail.com>
2022-09-27 12:42:22 -0300

    elimino importaciones que no se usan

--- src/components/footer/MainFooter.tsx
+++ src/components/footer/MainFooter.tsx
@@ -1,6 +1,5 @@
 import { faFacebookF, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
